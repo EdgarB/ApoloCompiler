9e9e4765d11a8b4a40b384aa5fcd531e362fd44c
Edgar Bustillos <edgar_daniel_1996@hotmail.com>
2018-04-23 14:29:58 -0500

    Algunos estatutos lineales incorporados en la maquina virtual

--- mVirtual.py
+++ mVirtual.py
@@ -1,5 +1,9 @@
+#Librerias
 import sys;
 import os;
+import pymunk;
+import time;
+import pyglet;
 from classes.pila import Pila
 
 #Arreglos que almacenan informacion
@@ -20,10 +24,26 @@ iComienzoMemFig = 81000;
 pilaEstadosCuad = Pila();
 
 #Arreglo con informacion de caudruplos
-liCuadruplos = []
+liCuadruplos = [];
 
 #Apuntador para leer cuadruplos
 iApuntadorCuads = 0;
+
+#VAriables para pyglet y Pymunk
+ventana = None;
+espacio = None;
+
+#Funcion para encontrar si la variable es un string
+def encontrarDobleEncomillado(line):
+    cantidadDobleComillas = 0;
+    for character in line:
+        if character == '"':
+            cantidadDobleComillas += 1;
+
+    if cantidadDobleComillas == 2:
+        return True;
+    else:
+        return False;
 
 #Leer archivo de cuadruplos y de constantes, guardarlos en sus arreglos
 # correspondientes.
@@ -45,12 +65,26 @@ if __name__ == '__main__':
         for line in f:
             cuad = line.replace('\n','').split(",");
             liCuadruplos.append(cuad);
-            print(cuad);
+            #print(cuad);
 
     with open(archivoCte) as f:
         for line in f:
+
+            line = line.replace('\n','');
+
+            if line == "Verdadero" or line == "verdadero" or line == "True" or line == "true":
+                line = True;
+            elif line == "Falso" or line == "falso" or line == "False" or line == "false":
+                line = False;
+            elif encontrarDobleEncomillado(line) == True:
+                line = line.replace('"','');
+            elif line.find(".") == 1:
+                line = float(line);
+            else:
+                line = int(line);
+
             liMemCte.append(line);
-            print(line);
+            #print(line);
 
 
 def getValor(iDirMem):
@@ -67,18 +101,53 @@ def getValor(iDirMem):
     global iComienzoMemCte;
     global iComienzoMemFig;
 
-    if(iDirMem < iComienzoMemLocal): #Global
-        return liMemGlobal[iDirMem - iComienzoMemGlobal];
-    elif(iDirMem < iComienzoMemTemporal): #Local
-        return liMemLocal[iDirMem - iComienzoMemLocal];
-    elif(iDirMem < iComienzoMemCte): #Temporal
-        return liMemTemporal[iDirMem - iComienzoMemTemporal];
-    elif(iDirMem < iComienzoMemFig): #Cte
-        return liMemCte[iDirMem - iComienzoMemCte];
-    else: #Figura
-        return liMemFig[iDirMem - iComienzoMemFig];
+    if(iDirMem[0]  == "("):
+        limiteInd = len(iDirMem) - 1;
+        iDirMem = getValor(iDirMem[1:limiteInd]);
+    iDirMem = int(iDirMem);
 
-def setValor(iDirMemoria, valor):
+    #Checar que no se None antes de hacer el return
+
+
+    if(iDirMem < iComienzoMemLocal): #Global
+        temporal = liMemGlobal[iDirMem - iComienzoMemGlobal];
+        if temporal != None:
+            return temporal;
+        else:
+            print("Error: Variable global no inicializada.");
+            return None;
+    elif(iDirMem < iComienzoMemTemporal): #Local
+        temporal = liMemLocal[iDirMem - iComienzoMemLocal];
+        if temporal != None:
+            return temporal;
+        else:
+            print("Error: Variable local no inicializada.");
+            return None;
+    elif(iDirMem < iComienzoMemCte): #Temporal
+        temporal = liMemTemporal[iDirMem - iComienzoMemTemporal];
+        if temporal != None:
+            return temporal;
+        else:
+            #print(iDirMem);
+            print("Error: Variable temporal no inicializada.");
+            return None;
+    elif(iDirMem < iComienzoMemFig): #Cte
+        temporal = liMemCte[iDirMem - iComienzoMemCte];
+        if temporal != None:
+            return temporal;
+        else:
+            print("Error: Variable constante no inicializada.");
+            return None;
+    else: #Figura
+        temporal = liMemFig[iDirMem - iComienzoMemFig];
+        if temporal != None:
+            return temporal;
+        else:
+            print("Error: Variable de figura no inicializada.");
+            return None;
+
+def setValor(iDirMem, valor):
+
     global liMemGlobal;
     global liMemLocal;
     global liMemTemporal;
@@ -90,16 +159,45 @@ def setValor(iDirMemoria, valor):
     global iComienzoMemTemporal;
     global iComienzoMemCte;
     global iComienzoMemFig;
+    if(iDirMem[0]  == "("):
+        limiteInd = len(iDirMem) - 1;
+        iDirMem = getValor(iDirMem[1:limiteInd]);
+    iDirMem = int(iDirMem);
 
     if(iDirMem < iComienzoMemLocal): #Global
+        iSize = len(liMemGlobal);
+        if iSize < iDirMem:
+            while(iSize < iDirMem):
+                liMemGlobal.append(None);
+                iSize += 1;
         liMemGlobal[iDirMem - iComienzoMemGlobal] = valor;
     elif(iDirMem < iComienzoMemTemporal): #Local
+        iSize = len(liMemLocal);
+        if iSize < iDirMem:
+            while(iSize < iDirMem):
+                liMemLocal.append(None);
+                iSize += 1;
         liMemLocal[iDirMem - iComienzoMemLocal] = valor;
     elif(iDirMem < iComienzoMemCte): #Temporal
+        iSize = len(liMemTemporal);
+        if iSize < iDirMem:
+            while(iSize < iDirMem):
+                liMemTemporal.append(None);
+                iSize += 1;
         liMemTemporal[iDirMem - iComienzoMemTemporal] = valor;
     elif(iDirMem < iComienzoMemFig): #Cte
+        iSize = len(liMemCte);
+        if iSize < iDirMem:
+            while(iSize < iDirMem):
+                liMemCte.append(None);
+                iSize += 1;
         liMemCte[iDirMem - iComienzoMemCte] = valor;
     else: #Figura
+        iSize = len(liMemFig);
+        if iSize < iDirMem:
+            while(iSize < iDirMem):
+                liMemFig.append(None);
+                iSize += 1;
         liMemFig[iDirMem - iComienzoMemFig] = valor;
 
 #Lector de caudruplos
@@ -110,34 +208,362 @@ while tempCuad[0] != "END":
         indOperando1 = tempCuad[1];
         indOperando2 = tempCuad[2];
         indResultado = tempCuad[3];
-        """
-            #Obtener si los numeros son int o float y convertilos
-            operando1 = getValor(indOperando1);
-            operando2 = getValor(indOperando2);
-            resultado = operando1 * operando2;
-            setValor(indResultado, resultado);
-        """
 
+        operando1 = getValor(indOperando1);
+        operando2 = getValor(indOperando2);
+
+        if operando1 == None or operando2 == None:
+            if operando1 == None:
+                print("Error: Operando izquierdo de la multiplicacion no tiene un valor.")
+            if operando2 == None:
+                print("Error: Operando derecho de la multiplicacion no tiene un valor.")
+            break;
+
+        resultado = operando1 * operando2;
+        setValor(indResultado, resultado);
 
     elif(instruccion == "+"):
 
+        indOperando1 = tempCuad[1];
+        indOperando2 = tempCuad[2];
+        indResultado = tempCuad[3];
+
+        operando1 = getValor(indOperando1);
+        operando2 = getValor(indOperando2);
+
+        if operando1 == None or operando2 == None:
+            if operando1 == None:
+                print("Error: Operando izquierdo de la suma no tiene un valor.")
+            if operando2 == None:
+                print("Error: Operando derecho de la suma no tiene un valor.")
+            break;
+
+        resultado = operando1 + operando2;
+        setValor(indResultado, resultado);
+
     elif(instruccion == "-"):
+        indOperando1 = tempCuad[1];
+        indOperando2 = tempCuad[2];
+        indResultado = tempCuad[3];
+
+        operando1 = getValor(indOperando1);
+        operando2 = getValor(indOperando2);
+
+        if operando1 == None or operando2 == None:
+            if operando1 == None:
+                print("Error: Operando izquierdo de la resta no tiene un valor.")
+            if operando2 == None:
+                print("Error: Operando derecho de la resta no tiene un valor.")
+            break;
+
+        resultado = operando1 - operando2;
+        setValor(indResultado, resultado);
 
     elif(instruccion == "/"):
+        indOperando1 = tempCuad[1];
+        indOperando2 = tempCuad[2];
+        indResultado = tempCuad[3];
 
-    """
-        >
-        <
-        <=
-        >=
-        ==
-        !=
-        &&
-        ||
-        =
-        +=
-        -=
-    """
+        operando1 = getValor(indOperando1);
+        operando2 = getValor(indOperando2);
 
-    iApuntadorCuads += 1;
+        if operando1 == None or operando2 == None:
+            if operando1 == None:
+                print("Error: Operando izquierdo de la division no tiene un valor.")
+            if operando2 == None:
+                print("Error: Operando derecho de la division no tiene un valor.")
+            break;
+
+        resultado = operando1 / operando2;
+        setValor(indResultado, resultado);
+
+    elif(instruccion == ">"):
+        indOperando1 = tempCuad[1];
+        indOperando2 = tempCuad[2];
+        indResultado = tempCuad[3];
+
+        operando1 = getValor(indOperando1);
+        operando2 = getValor(indOperando2);
+
+        if operando1 == None or operando2 == None:
+            if operando1 == None:
+                print("Error: Operando izquierdo de la comparacion - mayor que - no tiene un valor.")
+            if operando2 == None:
+                print("Error: Operando derecho de la comparacion - mayor que - no tiene un valor.")
+            break;
+
+        if operando1 > operando2:
+            resultado = True;
+        else:
+            resultado = False;
+
+        setValor(indResultado, resultado);
+
+    elif(instruccion == "<"):
+        indOperando1 = tempCuad[1];
+        indOperando2 = tempCuad[2];
+        indResultado = tempCuad[3];
+
+        operando1 = getValor(indOperando1);
+        operando2 = getValor(indOperando2);
+
+        if operando1 == None or operando2 == None:
+            if operando1 == None:
+                print("Error: Operando izquierdo de la comparacion - menor que - no tiene un valor.")
+            if operando2 == None:
+                print("Error: Operando derecho de la comparacion - menor que - no tiene un valor.")
+            break;
+
+        if operando1 < operando2:
+            resultado = True;
+        else:
+            resultado = False;
+
+        setValor(indResultado, resultado);
+
+    elif(instruccion == "<="):
+        indOperando1 = tempCuad[1];
+        indOperando2 = tempCuad[2];
+        indResultado = tempCuad[3];
+
+        operando1 = getValor(indOperando1);
+        operando2 = getValor(indOperando2);
+
+        if operando1 == None or operando2 == None:
+            if operando1 == None:
+                print("Error: Operando izquierdo de la comparacion - menor o igual que - no tiene un valor.")
+            if operando2 == None:
+                print("Error: Operando derecho de la comparacion - menor o igual que - no tiene un valor.")
+            break;
+
+        if operando1 <= operando2:
+            resultado = True;
+        else:
+            resultado = False;
+
+        setValor(indResultado, resultado);
+
+    elif(instruccion == ">="):
+        indOperando1 = tempCuad[1];
+        indOperando2 = tempCuad[2];
+        indResultado = tempCuad[3];
+
+        operando1 = getValor(indOperando1);
+        operando2 = getValor(indOperando2);
+
+        if operando1 == None or operando2 == None:
+            if operando1 == None:
+                print("Error: Operando izquierdo de la comparacion - mayor o igual que - no tiene un valor.")
+            if operando2 == None:
+                print("Error: Operando derecho de la comparacion - mayor o igual que - no tiene un valor.")
+            break;
+
+        if operando1 >= operando2:
+            resultado = True;
+        else:
+            resultado = False;
+
+        setValor(indResultado, resultado);
+
+    elif(instruccion == "=="):
+        # BOOLEANO - BOOLEANO
+        # FLOTANTE - FLOTANTE
+        # FLOTANTE - ENTERO
+        # ENTERO - FLOTANTE
+        # ENTERO - ENTERO
+        indOperando1 = tempCuad[1];
+        indOperando2 = tempCuad[2];
+        indResultado = tempCuad[3];
+
+        operando1 = getValor(indOperando1);
+        operando2 = getValor(indOperando2);
+
+        if operando1 == None or operando2 == None:
+            if operando1 == None:
+                print("Error: Operando izquierdo de la igualacion no tiene un valor.")
+            if operando2 == None:
+                print("Error: Operando derecho de la igualacion no tiene un valor.")
+            break;
+
+        if operando1 == operando2:
+            resultado = True;
+        else:
+            resultado = False;
+
+        setValor(indResultado, resultado);
+
+    elif(instruccion == "!="):
+        # BOOLEANO - BOOLEANO
+        # FLOTANTE - FLOTANTE
+        # FLOTANTE - ENTERO
+        # ENTERO - FLOTANTE
+        # ENTERO - ENTERO
+        indOperando1 = tempCuad[1];
+        indOperando2 = tempCuad[2];
+        indResultado = tempCuad[3];
+
+        operando1 = getValor(indOperando1);
+        operando2 = getValor(indOperando2);
+
+        if operando1 == None or operando2 == None:
+            if operando1 == None:
+                print("Error: Operando izquierdo de la no equivalencia no tiene un valor.")
+            if operando2 == None:
+                print("Error: Operando derecho de la no equivalencia no tiene un valor.")
+            break;
+
+        if operando1 != operando2:
+            resultado = True;
+        else:
+            resultado = False;
+
+        setValor(indResultado, resultado);
+
+    elif(instruccion == "&&"):
+
+        indOperando1 = tempCuad[1];
+        indOperando2 = tempCuad[2];
+        indResultado = tempCuad[3];
+
+        operando1 = getValor(indOperando1);
+        operando2 = getValor(indOperando2);
+
+        if operando1 == None or operando2 == None:
+            if operando1 == None:
+                print("Error: Operando izquierdo de la operacion AND no tiene un valor.")
+            if operando2 == None:
+                print("Error: Operando derecho de la operacion AND no tiene un valor.")
+            break;
+
+        if operando1 and operando2:
+            resultado = True;
+        else:
+            resultado = False;
+
+        setValor(indResultado, resultado);
+
+    elif(instruccion == "||"):
+        indOperando1 = tempCuad[1];
+        indOperando2 = tempCuad[2];
+        indResultado = tempCuad[3];
+
+        operando1 = getValor(indOperando1);
+        operando2 = getValor(indOperando2);
+
+        if operando1 == None or operando2 == None:
+            if operando1 == None:
+                print("Error: Operando izquierdo de la operacion OR no tiene un valor.")
+            if operando2 == None:
+                print("Error: Operando derecho de la operacion OR no tiene un valor.")
+            break;
+
+        if operando1 or operando2:
+            resultado = True;
+        else:
+            resultado = False;
+
+        setValor(indResultado, resultado);
+    elif(instruccion == "+="):
+
+        indOperando1 = tempCuad[1];
+        indResultado = tempCuad[3];
+
+        operando1 = getValor(indOperando1);
+        resultado = getValor(indResultado);
+
+        if operando1 == None:
+            print("Error: Operando derecho del operador mas igual no tiene un valor.")
+            break;
+
+        resultado = resultado + operando1;
+        setValor(indResultado, resultado);
+
+    elif(instruccion == "-="):
+
+        indOperando1 = tempCuad[1];
+        indResultado = tempCuad[3];
+
+        operando1 = getValor(indOperando1);
+        resultado = getValor(indResultado);
+
+        if operando1 == None:
+            print("Error: Operando derecho del operador menos igual no tiene un valor.")
+            break;
+
+        resultado = resultado - operando1;
+        setValor(indResultado, resultado);
+
+    elif(instruccion == "="):
+
+        indOperando1 = tempCuad[1];
+        indResultado = tempCuad[3];
+
+        operando1 = getValor(indOperando1);
+
+        if operando1 == None:
+            print("Error: Operando derecho del operador asignacion no tiene un valor.")
+            break;
+
+        setValor(indResultado, operando1);
+    elif(instruccion == "pantalla"):
+        global ventana;
+        indOperando1 = tempCuad[1];
+        indOperando2 = tempCuad[2];
+
+        operando1 = getValor(indOperando1);
+        if operando1 == None:
+            print("Error: Primer parametro de la funcion pantalla no tiene un valor.")
+            break;
+
+        operando2 = getValor(indOperando2);
+        if operando2 == None:
+            print("Error: Segundo parametro de la funcion pantalla no tiene un valor.")
+            break;
+
+        ventana = pyglet.window.Window(operando1,operando2, "Apolo", resizable=False);
+    elif(instruccion == "gravedad"):
+        global espacio;
+
+        indOperando1 = tempCuad[1];
+        indOperando2 = tempCuad[2];
+
+        operando1 = getValor(indOperando1);
+        if operando1 == None:
+            print("Error: Primer parametro de la funcion gravedad no tiene un valor.")
+            break;
+
+        operando2 = getValor(indOperando2);
+        if operando2 == None:
+            print("Error: Segundo parametro de la funcion gravedad no tiene un valor.")
+            break;
+
+        espacio = pymunk.Space();
+        espacio.gravity = operando1, operando2;
+    elif(instruccion == "cast"):
+        indOperando1 = tempCuad[1];
+        tipoCast = tempCuad[2];
+        indResultado = tempCuad[3];
+
+        operando1 = getValor(indOperando1);
+        if operando1 == None:
+            print("Debug error: Primer parametro de la instruccion cast no tiene un valor.")
+            break;
+
+        if(tipoCast == "int"):
+            valor = int(operando1);
+        elif(tipoCast == "float"):
+            valor = float(operando1);
+
+        setValor(indResultado, valor);
+
+
+
+    if(instruccion == "goto"):
+        Operando1 = tempCuad[3];
+        iApuntadorCuads = int(Operando1);
+    else:
+        iApuntadorCuads += 1;
+
+
+
     tempCuad = liCuadruplos[iApuntadorCuads];
---
+++ simTest.py
@@ -0,0 +1,37 @@
+import os;
+import sys;
+import pymunk;
+import time;
+import pyglet;
+from pymunk.pyglet_util import DrawOptions
+
+window = pyglet.window.Window(1280,720, "Pymunk test", resizable=False);
+options = DrawOptions()
+
+
+space = pymunk.Space();
+space.gravity = 0, -1000;
+
+poly = pymunk.Poly.create_box(None, size=(25,25));
+moment = pymunk.moment_for_poly(10,poly.get_vertices())
+print(moment)
+body = pymunk.Body(1,moment, pymunk.Body.DYNAMIC);
+poly.body = body
+body.position = 640,700;
+
+
+space.add(body, poly);
+body = None;
+poly = None;
+
+@window.event
+def on_draw():
+    window.clear();
+    space.debug_draw(options);
+
+def update(dt):
+    space.step(dt)
+
+if __name__ == "__main__":
+    pyglet.clock.schedule_interval(update, 1.0/60);
+    pyglet.app.run();
